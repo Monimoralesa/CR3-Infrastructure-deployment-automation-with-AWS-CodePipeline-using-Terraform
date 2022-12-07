19bbdb7f8c5819160a46bb0d999a95a9941777b0
Monimoralesa <68556552+Monimoralesa@users.noreply.github.com>
2022-12-06 22:16:48 -0500

    Delete Terraform directory

--- Terraform/status.tf
+++
@@ -1,7 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket   = "aws-terrafrom-pipeline-cr3-base"
-    key      = "stack_dev-base.tfstate"
-    region   = "us-east-1"
-  }
-}
--- Terraform/userdata.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-sudo yum update -y
-sudo yum install ruby -y
-sudo yum install wget -y
-cd /home/ec2-user
-wget https://aws-codedeploy-us-east-1.s3.us-east-1.amazonaws.com/latest/install
-chmod +x ./install
-sudo ./install auto
