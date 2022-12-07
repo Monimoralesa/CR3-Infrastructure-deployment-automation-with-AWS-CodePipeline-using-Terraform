44e462cc9d8cfdb9070fe7897c4a22622a5a5a6c
Monimoralesa <68556552+Monimoralesa@users.noreply.github.com>
2022-12-06 22:06:56 -0500

    Create userdata.sh

---
+++ Scripts/Terraform/userdata.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+sudo yum update -y
+sudo yum install ruby -y
+sudo yum install wget -y
+cd /home/ec2-user
+wget https://aws-codedeploy-us-east-1.s3.us-east-1.amazonaws.com/latest/install
+chmod +x ./install
+sudo ./install auto
